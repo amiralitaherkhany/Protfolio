b5cf7e040c7af345ba2d3f64f686aa6b5753eb68
amiralitaherkhany <amiralitaherkhany07@gmail.com>
2025-08-01 17:22:29 +0330

    refactor: extract app widget to separate file
    Move the main app widget to a dedicated file (app.dart) for better code organization and maintainability. Update main.dart and widget_test.dart to use the new App widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:my_portfolio/pages/main_page.dart';
+import 'package:my_portfolio/app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Amirali\'s Portfolio',
-      debugShowCheckedModeBanner: false,
-      home: MainPage(),
-    );
-  }
+  runApp(const App());
 }
