961f4241c8ce650f096f0fd80c05a85d94ce5ab2
amiralitaherkhany <amiralitaherkhany@users.noreply.github.com>
2025-08-25 19:04:59 +0000

    deploy: e1fa42a6f895cba3704cd02f45e758170caeb88d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1118493355"
+    serviceWorkerVersion: "2398125321"
   }
 });
