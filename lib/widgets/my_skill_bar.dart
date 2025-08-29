e57ccaae489e4f693f08c3448e541ce66f5b34c1
amiralitaherkhany <amiralitaherkhany07@gmail.com>
2025-08-29 23:31:36 +0330

    feat: replace flutter_svg with vector_graphics for SVG rendering
    - Add SVG assets for various technologies (kotlin, flutter, dart, git, github, githubactions, go)
    - Update pubspec.yaml to use vector_graphics instead of flutter_svg
    - Modify MySkillBar widget to use VectorGraphic with AssetBytesLoader

--- lib/widgets/my_skill_bar.dart
+++ lib/widgets/my_skill_bar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:my_portfolio/extensions/context_extensions.dart';
 import 'package:my_portfolio/theme/dark_colors.dart';
+import 'package:vector_graphics/vector_graphics.dart';
 
 class MySkillBar extends StatelessWidget {
   const MySkillBar({
@@ -30,12 +30,10 @@ class MySkillBar extends StatelessWidget {
               ),
               color: DarkColors.myWhite,
             ),
-            child: Center(
-              child: SvgPicture.network(
-                "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/${skill.name}/${skill.name}-original.svg",
-                width: 40,
-                height: 40,
-              ),
+            child: VectorGraphic(
+              loader: AssetBytesLoader('assets/svg/${skill.name}.svg'),
+              width: 40,
+              height: 40,
             ),
           ),
           Expanded(
