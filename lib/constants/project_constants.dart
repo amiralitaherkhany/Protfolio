1e390cc1176fe9488366558c5d9b8c9c7f49e773
amiralitaherkhany <amiralitaherkhany07@gmail.com>
2025-10-23 17:32:36 +0330

    ✨ feat(project): add apple shop project
    - introduce apple shop project with details
    - include repo link, name, description, screenshots, and skill names

--- lib/constants/project_constants.dart
+++ lib/constants/project_constants.dart
@@ -1,5 +1,3 @@
-import 'package:my_portfolio/widgets/my_skill_bar.dart';
-
 enum ProjectConstants {
   valorantIntel(
     repo: "https://github.com/amiralitaherkhany/valorant-intel",
@@ -36,6 +34,23 @@ enum ProjectConstants {
       "kotlin",
       "jetpackcompose",
       "githubactions",
+    ],
+  ),
+  appleshop(
+    repo: "https://github.com/amiralitaherkhany/apple-shop",
+    name: "Apple Shop",
+    description:
+        "A sample Flutter shopping app that demonstrates a simple and responsive design. Users can browse products, view details, and add items to their cart.",
+    screenShots: [
+      "${_baseUrl}apple-shop/master/screenshots/1.png",
+      "${_baseUrl}apple-shop/master/screenshots/2.png",
+      "${_baseUrl}apple-shop/master/screenshots/3.png",
+      "${_baseUrl}apple-shop/master/screenshots/4.png",
+      "${_baseUrl}apple-shop/master/screenshots/5.png",
+    ],
+    skillNames: [
+      "dart",
+      "flutter",
     ],
   );
 
